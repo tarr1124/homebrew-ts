185326345978e486d98eb9ea798d9412bb9e1d6d
tarr1124 takahiro.tokunaga <takahiro.tokunaga@plaid.co.jp>
2018-04-12 11:17:17 +0900

    fix version and for tarr

--- ts.rb
+++ ts.rb
@@ -1,11 +1,11 @@
 require 'formula'
-HOMEBREW_TS_VERSION='0.3.3'
+HOMEBREW_TS_VERSION='0.3.2'
 
 class Ts < Formula
-  homepage 'https://github.com/timakin/ts'
-  url 'https://github.com/timakin/ts.git', :tag => "v#{HOMEBREW_TS_VERSION}"
+  homepage 'https://github.com/tarr1124/ts'
+  url 'https://github.com/tarr1124/ts.git', :tag => "v#{HOMEBREW_TS_VERSION}"
   version HOMEBREW_TS_VERSION
-  head 'https://github.com/timakin/ts.git', :branch => 'master'
+  head 'https://github.com/tarr1124/ts.git', :branch => 'master'
 
   depends_on 'go' => :build
   depends_on 'hg' => :build
@@ -17,9 +17,9 @@ class Ts < Formula
     system 'go', 'get', 'github.com/codegangsta/cli'
     system 'go', 'get', 'github.com/jzelinskie/geddit'
     system 'go', 'get', "github.com/getwe/figlet4go"
-    system 'go', 'get', 'github.com/timakin/ts'
-    mkdir_p buildpath/'src/github.com/timakin/ts'
-    ln_s buildpath, buildpath/'src/github.com/timakin/ts'
+    system 'go', 'get', 'github.com/tarr1124/ts'
+    mkdir_p buildpath/'src/github.com/tarr1124/ts'
+    ln_s buildpath, buildpath/'src/github.com/tarr1124/ts'
     system 'go', 'build', '-o', 'ts'
     bin.install 'ts'
   end
